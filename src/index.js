9734e488bd844b42a4b327e0f0c8e82aeb82dc08
Iuliia Plekhanova <julec2006@yandex.ru>
2020-02-20 16:28:38 +0400

    fix: add solution to index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
 
 exports.min = function min (array) {
+    if (array != undefined && array.length !=0) {
+    let min = array[0];
+    for (let item of array) {
+        if (item < min) min = item;
+    }
+    return min;
+}    
   return 0;
 }
 
 exports.max = function max (array) {
+    if (array != undefined && array.length !=0) {
+    let max=array[0];
+    for (let item of array) {
+        if (item>max) max = item;
+    }
+    return max;
+}    
   return 0;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (array != undefined && array.length !=0) {
+    let sum=0;
+    for (let item of array) {
+        sum+=item;
+    }
+  return sum/array.length;
+}
+return 0;
 }
